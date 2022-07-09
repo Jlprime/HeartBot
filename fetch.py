549c8405752b296af560724ff3599708bd9e3f76
Muzi <flyingarticlesfg@gmail.com>
2022-07-09 22:33:57 +0800

    updated fetch.py

--- fetch.py
+++ fetch.py
@@ -1,4 +1,14 @@
-from sqlalchemy import create_engine
+import string
+import sqlalchemy as db
 
-engine = create_engine("sqlite:///scraped_data.db")
+engine = db.create_engine("sqlite:///scraped_data.db")
 
+def database_init(directory, table_name):
+    engine = db.create_engine("sqlite:///scraped_data.db")
+    headers = db.Table(table_name, metadata=db.MetaData()).columns.keys()
+    return engine, headers
+
+# with engine.connect() as connection:
+#     COMMAND = f'SELECT * FROM VolunteerOpportunities WHERE EventDate = "{date}"'
+#     resultProxy = connection.execute(COMMAND)
+#     resultFetch = resultProxy.fetchall()
